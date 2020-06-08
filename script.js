49d1fff882a6e5e9256e912f7d737af965d869e1
PCU <PCU@PCUs-Mac-mini.local>
2020-06-08 14:49:38 -0400

    first draft completed

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 
 // everything inside tarot object
     const tarot = {
-// array of cards as property of tarot object
+// array of cards as property of object
+        
             cards: [
                 '00_Fool.jpg" alt="the fool"',
                 '01_Magician.jpg" alt="the magician"',
@@ -27,13 +28,16 @@ $(document).ready(function(){
                 '20_Judgement.jpg" alt="judgement"',
                 '21_World.jpg" alt="the world"'
                 ],
+
+
+
 //init method kicks off chain of functions, firstly 'shuffle'
             init: function() {
                 tarot.shuffle();
             },
 
             shuffle: function() {
-//shuffle method runs through the length of the array and re-orders them (method taken from youtube 'pseudo-randomizer' tutorial)
+//shuffle method runs through the length of the array and re-orders them (method taken from youtube 'shuffling an array' tutorial NOTE: i can't quite wrap my head around how this works, but in this application it works)
                 let random = 0;
                 let temp = 0;
                 for(i = 1; i < tarot.cards.length; i++) {
@@ -41,6 +45,7 @@ $(document).ready(function(){
                     temp = tarot.cards[i];
                     tarot.cards[i] = tarot.cards[random];
                     tarot.cards[random] = temp;
+                 
                 }
 //assignCards method is called after shuffle is completed
                 tarot.assignCards();
@@ -49,12 +54,21 @@ $(document).ready(function(){
             assignCards: function(){
                 $('.card').each(function(index){
                     $(this).attr('data-card-value', tarot.cards[index]);
+
+
+                    // attempt to attach alt tags in different way:
+//                     $(this).attr('data-card-alt', tarot.alt[index])
                 });
+
+
+
 //click events are then called once data-card-value attribute has been assigned
+
                 tarot.clickEvents();
+             
             },
 
-//click handler waits for click event on .card elements, on click the html is updated with image element and filetype enveloping name of array item (which is named the same as the image). "unselected" is dormant class on all .card elements, which is then removed and replaecd with 'selected', image element fadesIn then calls back for other elements to fadeOut
+//click handler waits for click event on .card elements, on click the html is updated with image element and filetype enveloping name of array item (which is named the same as the image). "unselected" is dormant class on all .card elements, which is then removed and replaced with 'selected'. NOTE: javascript automatically changes data-card-value to cardValue
 
 
             clickEvents: function(){
@@ -62,7 +76,8 @@ $(document).ready(function(){
                     $(this).html('<img src="./assets/' + $(this).data('cardValue') + ">"); 
                     // console.log($(this).data('cardValue'));     
                     $(this).removeClass('unselected').addClass('selected');
-                    // console.log($('.selected').length)
+                    console.log($('.selected').length)
+                    //conditional statement checks to see if amount of items with a class of .selected is three, if so the other elements are removed from mark up and the three selected items grow in size
                     $(this).fadeIn(function(){
                         if ($('.selected').length === 3) {  
                             $('.unselected').css("display", "none");
@@ -80,7 +95,7 @@ $(document).ready(function(){
                     if ((event.keyCode || event.which) == 13) {
                         console.log(this);
                         $(this).trigger('click');
-                    }
+                    }       
                 });
             }            
     };
